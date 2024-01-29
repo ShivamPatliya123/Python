fbd7596d4610922c18e903044718bc7f786d6bc6
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-01-29 20:50:06 +0530

    01print.py

---
+++ 01print.py
@@ -0,0 +1,15 @@
+print("hello")
+a=1;
+print(a)
+a=a+1;
+print(a)
+a=a+1;
+print(a)
+a=a+1;
+print(a)
+a=a+1;
+print(a)
+
+#"second type of writting the code;"
+
+a=1;print(a);a=a+1;print(a);a=a+1;print(a);a=a+1;print(a);a=a+1;print(a)
