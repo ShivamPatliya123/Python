2da9687867f5cc86860861ce17c8f4d9dcbf4570
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-01-31 17:40:24 +0530

    04ifcondition.py

---
+++ 04ifcondition.py
@@ -0,0 +1,9 @@
+print("hi how are you")
+c=input("Enter the number")
+d=int(c)
+if(d==1):
+    print("You Entered One")
+if(d==2):
+    print("You Entered Two")
+if(d==3):
+    print("You Entered Three")
