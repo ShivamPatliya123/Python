b0cab76ea3833ed12b797cf6cd6a2ab709565835
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-02-12 22:52:32 +0530

    06Simple.py

---
+++ 06Simple.py
@@ -0,0 +1,4 @@
+print("hey",6,7,sep="--",end="005\n")
+print("hello")
+print("hey Iam a good boy \n and this viewer is also a good boy&girl ")
+print("hey Iam a \"good boy\" \n and this viewer is also a \"good boy&girl\" ")
