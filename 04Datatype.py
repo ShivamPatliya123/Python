c091209d67c5605cea8aa5a0f21376e4f0484b4a
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-02-12 22:50:41 +0530

    04Datatype.py

---
+++ 04Datatype.py
@@ -0,0 +1,12 @@
+a=10
+b=True
+c="shivam"
+d=1.2
+e=complex(4,5)
+f=None
+print("type of a",type(a))
+print("type of b",type(b))
+print("type of c",type(c))
+print("type of d",type(d))
+print("type of e",type(e))
+print("type of f",type(f))
