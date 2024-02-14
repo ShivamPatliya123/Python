9461c98153bfa5ed4ac245e59a7871c445feb346
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-02-14 21:50:00 +0530

    07forloop.py

---
+++ 07forloop.py
@@ -0,0 +1,9 @@
+# for i in range(11):
+    # print("i exicute in 0 to 10 time",i)
+answer = 0
+for i in range(11):
+    answer = answer + i
+    # step by step print answer
+    print(answer)
+#then direct answer is print 
+print(answer)
