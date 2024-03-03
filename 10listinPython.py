6825547cef3017cfb1d831fbd582e5fac016dba4
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-03-03 20:39:27 +0530

    10listinPython.py

---
+++ 10listinPython.py
@@ -0,0 +1,15 @@
+shopping=["Bread","Coffee","Sugar"]
+print(shopping)#computer friendly command:
+for item in shopping:
+    print(item)#user friendly command:
+
+
+#add a new element in list:
+# used append function:
+shopping.append("curd")
+print(shopping)
+
+
+#add a new element with index value used in insert function.
+shopping.insert(1,"oil")
+print(shopping)
