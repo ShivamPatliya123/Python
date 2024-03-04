303c9c7489f46e53267d6eac90c869b402f90edf
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-03-04 22:21:49 +0530

    12String.py

---
+++ 12String.py
@@ -0,0 +1,13 @@
+student = ["Arun","Rajesh","Haris","Lakshmi","Shivam","Akanksha"]
+student.sort()
+print(student)
+
+#Add a new name
+
+student.insert(2,"Nikita")
+print(student)
+
+# slicing 
+
+student[3:]
+print (student)
