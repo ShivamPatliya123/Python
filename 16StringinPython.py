411e7482c44fa7e422bfcb70b745507814b67ce8
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-03-06 21:54:54 +0530

    16StringinPython.py

---
+++ 16StringinPython.py
@@ -0,0 +1,37 @@
+name = "shivam" # double coat
+friend = 'shiva' # single coat
+print(name)
+print(friend)
+print() # new line is creat
+
+
+# print a multiple line in python using a triple single or double coat 
+# single triple coat
+apple = '''he said 
+hi shivam
+hy i am good
+I want to eat an apple'''
+print(apple)
+print()
+
+#double triple caot
+
+apple="""he said 
+hi shivam
+hy i am good
+I want to eat an apple"""
+print(apple)
+print()
+
+
+# indexing
+
+
+print("indexing")
+name="shivam"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
