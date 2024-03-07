91acf8165374e087037848a1d1a31a999e737cb5
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-03-07 21:17:41 +0530

    17Slicing.py

---
+++ 17Slicing.py
@@ -0,0 +1,12 @@
+fruit="mango"
+len1=len(fruit)
+print(len1)
+# slicing
+print(fruit[0:5]) # including 0 but not 5
+print(fruit[1:4])# including 1 but not 4
+print(fruit[:5]) 
+# nagative slicing
+print(fruit[0:-3])
+print(fruit[:len(fruit)-3])
+print(fruit[-1:-3]) #not to be exicuit
+print(fruit[-3:-1])
