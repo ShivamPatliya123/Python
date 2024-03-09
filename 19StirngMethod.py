65d8a76bc557363e35db9c90eda66eaef246778a
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-03-09 21:22:34 +0530

    19StirngMethod.py

---
+++ 19StirngMethod.py
@@ -0,0 +1,47 @@
+# upper case()
+str="abcdeHE"
+print(str.upper())
+
+#Lower case()
+str="ABCDEfgh"
+print(str.lower())
+
+#rstrip()
+str1="Hello!!!!"
+print(str1.rstrip("!"))
+
+#replace()
+str2="shivam"
+print(str2.replace("shivam","shiva"))
+
+#split()
+str3="Shivam Gurjar"
+print(str3.split(" "))
+
+#Capitalize() first word of first sentance is capital
+str4="hello.How are you"
+print(str4.capitalize())
+
+#center()
+str5="Welcome to colsole"
+print(str5.center(50))
+
+#count()
+str6="shivam shivam shivam"
+print(str6.count("shivam"))
+
+#endswith() 
+str7="welcome to my program!!!"
+print(str7.endswith("!"))
+
+#find() give the index value
+str8="He's name is Dan,He is an honest man"
+print(str8.find("is"))
+
+#index()
+str9="He's name is Dan,He is an honest man"
+print(str9.index("Dan"))
+
+#isalnum
+str10="Welcometotheconsole"
+print(str10.isalnum())
