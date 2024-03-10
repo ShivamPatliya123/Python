d74d812f03835fc0b48550d7e70830a6692df464
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-03-10 21:13:59 +0530

    22ElifCondition.py

---
+++ 22ElifCondition.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the number"))
+if(a<0):
+    print("Number is Negative")
+elif(a==0):
+    print("Number is Zero")
+elif(a==999):
+    print("Number is Special")
+else:
+    print("Number is Positive")
