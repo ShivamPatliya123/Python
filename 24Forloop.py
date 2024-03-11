aa2aeda37dd3b185639e5a9542c694e437cc5040
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-03-11 21:40:37 +0530

    24Forloop.py

---
+++ 24Forloop.py
@@ -0,0 +1,18 @@
+#stirng print using for loop
+name="shivam"
+for i in name:
+    print(i,end=",")
+
+#list print using for loop
+
+colours=["Red","Yellow","Green","Blue"]
+for colour in colours:
+    print(colours)
+    for i in colour:
+       print(i)
+
+#Range()
+
+for k in range(1,11):
+    print(k*2)
+    
