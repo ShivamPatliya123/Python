dfef03be5326cc3666babb496188015cc7bf8f34
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-03-12 21:20:52 +0530

    25WhileLoop.py

---
+++ 25WhileLoop.py
@@ -0,0 +1,17 @@
+i=1
+while(i<=10):
+    print(i)
+    i=i+1
+print("Done with the loop")
+
+count = 10
+while(count>0):
+    print(count)
+    count=count-1
+
+i=1
+j=5
+while(i<=10):
+    print(i*j)
+    i=i+1
+print ("Complete the loop")
