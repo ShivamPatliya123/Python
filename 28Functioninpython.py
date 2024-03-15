e946074a12329736c78eca3ff212a7acf905c33b
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-03-15 21:25:23 +0530

    28Functioninpython.py

---
+++ 28Functioninpython.py
@@ -0,0 +1,17 @@
+def calculatemean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+        print("Frist number is greater")
+    else:
+        print("Second number is greater or equal")
+
+def isLesser(a,b):
+    pass
+
+a=9
+b=10
+isGreater(a,b)
+calculatemean(a,b)
