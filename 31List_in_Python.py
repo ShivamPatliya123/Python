c4567882a444f77807d424c75e7158ff9c08dd6e
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-03-20 21:12:25 +0530

    31List_in_Python.py

---
+++ 31List_in_Python.py
@@ -0,0 +1,16 @@
+l=['red','yellow','blue','green',32,3,2]
+print(type(l))
+print(l)
+print(l[0])
+print(l[1])
+print(l[2])
+print(l[3])
+print(l[4])
+print(len(l))
+
+#negative indexing
+
+colors=['red','yellow','blue','indigo','orange']
+print(colors)
+print(colors[-2])
+print(colors[len(colors)-3])
