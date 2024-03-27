9fd42fcec80ba95f0a608ce08fdca5eba0934e96
Shivam Patliya <142159496+ShivamPatliya123@users.noreply.github.com>
2024-03-27 21:16:18 +0530

    38Foor_Loop_Else.py

---
+++ 38Foor_Loop_Else.py
@@ -0,0 +1,16 @@
+# for i in range(5):
+#     print(i)
+# else:
+#     print("sorry no i")
+
+# for i in ():
+#     print()
+# else:
+#     print("sorry no i")
+
+for i in range(5):
+    print(i)
+    if (i==4):
+        break
+else:
+    print("sorry no i")
